f1573834d9b14308d9e6b72d3791450344c5c63a
Jose Fco Rodríguez Hernández <alu0100894646@ull.edu.es>
2018-05-01 19:28:10 +0100

    Te subo elrolloahi

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var MongoClient = require('mongodb').MongoClient;
 var fs = require('fs');
-var readline = require('readline');
+//var readline = require('readline');
 
 MongoClient.connect("mongodb://localhost:27017/", function(err, database)
 {
@@ -21,7 +21,8 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, database)
   console.log(aux2);
 
   var div = dividirProducto(aux2);
-  console.log(div[0]+"___________"+div[1]); 
+  console.log(div[0]+"___________"+div[1]);
+  console.log(parseFloat(div[1]));
 //SALTAMOS LA PRIMERA linea
   // FileProductos.toString().split(/\n/).forEach(function(line, index){
   //   if(index != 0)
@@ -34,12 +35,30 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, database)
   //});
      for(var i = 0; i < 1000; i++)
      {
+         console.log(i);
          var empleado_aux = dameEmpleado(FileEmpleados);
          var productos_aux =  dameProducto(FileProductos);
          var div = dividirProducto(productos_aux);
          
-         var Factura = {'Empleado':empleado_aux };
-         
+         var Factura = {'Empleado':empleado_aux};
+         var Detalle = [];
+         for(var P = 1; P <= random(1,5);P++)
+         {
+             var Cantidad = random(1,10);
+             //Hay que parsear a float el string 
+             var Precio = Cantidad * parseFloat(div[1]);
+             var Igic = Precio*0.07;
+             var Total = Precio+Igic;
+             Detalle.push({'Cantidad':Cantidad, 'Producto':div[0],'Importe':div[1],'IGIC':Igic,'Total':Total});
+             console.log(Detalle);
+             
+         }
+         //Habría que hacer algo aqui con el subtotal y todo eso, pero lo de arriba creo que funciona naisu;
+         /*Factura.SubTotal = SubTotal;
+         Factura.Igic = TotalIgic;
+         Factura.Total = TotalFactura;
+         Factura.Detalle = Detalle;
+         ColeccionFacturas.insert(Factura);*/
          
          
      }
@@ -63,6 +82,8 @@ MongoClient.connect("mongodb://localhost:27017/", function(err, database)
   //     ColeccionProductos.insert(Tupla);
   //}
 });
+
+//En el array 0 queda el nombre y el array 1 queda el precio
 
 function dividirProducto(producto,nombre_producto,precio)
 {
